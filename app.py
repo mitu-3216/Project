ab11edba6d48bd2373be81b59cc14ce8f9ed1210
mitu-3216 <81210783+mitu-3216@users.noreply.github.com>
2022-10-05 19:44:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,28 @@ if(st.button("Submit")):
 
     # You can scan 0-65535 ports. This scanner is basic and doesn't use multithreading so scanning all the ports is not advised.
 st.write("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
-port_range = st.text_input("Enter port range: ")
+port_range01 = st.text_input("From: ")
+port_range02 = st.text_input("To: ")
+
+port_min = int(port_range01)
+port_max = int (port_range02)
+
+
     # We pass the port numbers in by removing extra spaces that people sometimes enter. So if you enter 80 - 90 instead of 80-90 the program will still work.
 if(st.button("Click me")):
-    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    #port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
 
-    if port_range_valid:
+    #if port_range_valid:
             # We're extracting the low end of the port scanner range the user want to scan.
-            port_min = int(port_range_valid.group(1))
+            #port_min = int(port_range_valid.group(1))
             # We're extracting the upper end of the port scanner range the user want to scan.
-            port_max = int(port_range_valid.group(2))
+            #port_max = int(port_range_valid.group(2))
             
 
            
     # We're looping over all of the ports in the specified range.
-            for port in range(port_min, port_max + 1):
-                try:
+    for port in range(port_min, port_max + 1):
+        try:
                     # The result is quite interesting to look at. You may want to inspect the dictionary it returns. 
                     # It contains what was sent to the command line in addition to the port status we're after. 
                     # For in nmap for port 80 and ip 10.0.0.2 you'd run: nmap -oX - -p 89 -sV 10.0.0.2
@@ -52,6 +58,6 @@ if(st.button("Click me")):
                     # We extract the port status from the returned object
                     port_status = (result['scan'][ip_add_entered]['tcp'][port]['state'])
                     st.write(f"Port {port} is {port_status}")
-                except:
+        except:
                     # We cannot scan some ports and this ensures the program doesn't crash when we try to scan them.
                     st.write(f"Cannot scan port {port}.")
