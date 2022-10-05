33fb199a97e14c02990bb1a7e42f010ae5c9b6f2
mitu-3216 <81210783+mitu-3216@users.noreply.github.com>
2022-10-05 14:48:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import nmap
 
 import ipaddress
 
@@ -11,10 +12,10 @@ port_max = 65535
 
 while True:
     ip_add_entered = st.text_input("\nPlease enter the ip address that you want to scan: ")
-  
+    # If we enter an invalid ip address the try except block will go to the except block and say you entered an invalid ip address.
     try:
         ip_address_obj = ipaddress.ip_address(ip_add_entered)
-      
+        # The following line will only execute if the ip is valid.
         st.write("You entered a valid ip address.")
         break
     except:
@@ -22,28 +23,31 @@ while True:
 
 
 while True:
-    
+    # You can scan 0-65535 ports. This scanner is basic and doesn't use multithreading so scanning all the ports is not advised.
     st.write("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
-    if(st.button("Submi")):
-    port_range =st.text_input("Enter port range: ")
-   
+    port_range = st.text_input("Enter port range: ")
+    # We pass the port numbers in by removing extra spaces that people sometimes enter. So if you enter 80 - 90 instead of 80-90 the program will still work.
     port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
     if port_range_valid:
-        
+        # We're extracting the low end of the port scanner range the user want to scan.
         port_min = int(port_range_valid.group(1))
-      
+        # We're extracting the upper end of the port scanner range the user want to scan.
         port_max = int(port_range_valid.group(2))
         break
-scanner= ""
-#nm = nmap.PortScanner()
 
+nm = nmap.PortScanner()
+# We're looping over all of the ports in the specified range.
 for port in range(port_min, port_max + 1):
     try:
-       
+        # The result is quite interesting to look at. You may want to inspect the dictionary it returns. 
+        # It contains what was sent to the command line in addition to the port status we're after. 
+        # For in nmap for port 80 and ip 10.0.0.2 you'd run: nmap -oX - -p 89 -sV 10.0.0.2
         result = nm.scan(ip_add_entered, str(port))
-        
+        # Uncomment following line and look at dictionary
+        # print(result)
+        # We extract the port status from the returned object
         port_status = (result['scan'][ip_add_entered]['tcp'][port]['state'])
-        st.write(f"Port {port} is {port_status}")
+        print(f"Port {port} is {port_status}")
     except:
-       
+        # We cannot scan some ports and this ensures the program doesn't crash when we try to scan them.
         st.write(f"Cannot scan port {port}.")
